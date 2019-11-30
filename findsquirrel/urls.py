1d5b65d23dad8e65fda1a82b5430b7c6553e4f92
Yinuo <yz3729@columbia.edu>
2019-11-29 19:35:47 -0500

    Update urls, views

--- findsquirrel/urls.py
+++ findsquirrel/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('map/', views.map, name='map'),
+    path('sightings/', views.sightings, name='sightings'),
 ]
--- findsquirrel/views.py
+++ findsquirrel/views.py
@@ -9,5 +9,22 @@ from .form import SquirrelForm
 def index(request):
     return render(request, 'findsquirrel/index.html')
 
+def map(request):
+    latlong = list()
+    for i in Squirrel.objects.all():
+        l_dict = {}
+        l_dict['latitude']=i.latitude
+        l_dict['longitude']=i.longitude
+        latlong.append(l_dict)
+    return render(request, 'findsquirrel/map.html', {'latlong':latlong})
+
+def sightings(request):
+    squirrel_id = list()
+    for i in Squirrel.objects.all():
+        i_dict = {}
+        i_dict['sid']=i.squirrel_id
+        squirrel_id.append(i_dict)
+    return render(request, 'findsquirrel/sightings.html', {'squirrel_id':squirrel_id})
+
 # Create your views here.
 
